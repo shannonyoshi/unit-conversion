19ad2916a505e918696ada72de11351747ed3a12
Shannon <shannon.yoshikawa@gmail.com>
2020-04-03 16:48:13 -0400

    updated favicon/title, removed unnecessary files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <header className="App-header">
-          <h3 className="App-logo">Unit Converter</h3>
+          <h3 className="App-logo">Unit<br/>Converter</h3>
         </header>
     );
   }
